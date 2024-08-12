4b743338db08d0b7d43718b8486339f7582bf387
Nikhil Bola Kamath <nikhilkb98@gmail.com>
2024-08-11 23:15:57 -0500

    Initial commit

---
+++ ivc.py
@@ -0,0 +1,17 @@
+####################################################################################################
+# This file serves as the starting point for the application.
+# How to run the application: `python -m main`
+####################################################################################################
+
+from textual.app import App
+
+
+class IVCApp(App):
+
+    """A Textual application to serve the IVC (Intelligent-version-control) system."""
+
+    pass
+
+
+if __name__ == "__main__":
+    IVCApp().run()
